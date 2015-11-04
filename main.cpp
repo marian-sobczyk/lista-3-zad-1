72678b062ebd8240d34dcd06b5e638f22556b825
Marian Sobczyk <m.sobczyk@nomtek.com>
2015-11-04 13:43:17 +0100

    Hidding password

--- main.cpp
+++ main.cpp
@@ -6,38 +6,26 @@
 
 using namespace std;
 
-static const unsigned char key[] = {
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0,
-        0, 0
-};
-
 int main() {
+    char *password = getpass("Podaj hasło: ");
+    KeyChain *keyChain = new KeyChain((unsigned char *) "/Users/marian/Desktop/ssl/keyChain.kc",
+                                      (unsigned char *) password, 0);
+    unsigned char *key = keyChain->key;
     FileContent *inputContent = new FileContent(false);
     inputContent->readFromPath("/Users/marian/Desktop/ssl/test1.txt");
-    AESCTREncryptor *encryptor = new AESCTREncryptor((unsigned char *) key);
+    AESCTREncryptor *encryptor = new AESCTREncryptor(key);
     FileContent *outputContent = encryptor->encryptData(inputContent);
-    outputContent->saveInPath("/Users/marian/Desktop/ssl/test2.txt");
+//    outputContent->saveInPath("/Users/marian/Desktop/ssl/test2.txt");
     delete inputContent;
     inputContent = new FileContent(true);
     inputContent->readFromPath("/Users/marian/Desktop/ssl/test2.txt");
     delete encryptor;
-    encryptor = new AESCTREncryptor((unsigned char *) key);
+    encryptor = new AESCTREncryptor(key);
     delete outputContent;
     outputContent = encryptor->decryptData(inputContent);
     outputContent->saveInPath("/Users/marian/Desktop/ssl/test3.txt");
     delete encryptor;
     delete outputContent;
-
-    unsigned char *password = (unsigned char *) "Marian";
-
-    KeyChain *keyChain = new KeyChain((unsigned char *) "/Users/marian/Desktop/ssl/keyChain.kc", password, 0);
-
-
     delete keyChain;
     return 0;
 }
