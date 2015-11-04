734a786dce3675ba08ec2841af205b38cad04de1
Marian Sobczyk <m.sobczyk@nomtek.com>
2015-11-04 15:09:16 +0100

    Added parameters

--- main.cpp
+++ main.cpp
@@ -6,10 +6,46 @@
 
 using namespace std;
 
-int main() {
+enum EncodingType {
+    ETUnknown = 0,
+    ETCBC = 1,
+    ETCTR = 2
+};
+
+enum EncodingEffect {
+    EEUnknown = 0,
+    EEEncoding = 1,
+    EEDecoding = 2
+};
+
+EncodingType getEncodingType(char *encodingType);
+
+EncodingEffect getEncodingEffect(char *effectASCII);
+
+int main(int argc, char *argv[]) {
+    if (argc < 6) {
+        cout <<
+        "Użycie: tryb szyfrowania | ścieżka do keystora | identyfikator klucza | plik do zaszyfrowania | 1 - szyfrowanie, 0 - deszyfrowanie\n";
+        return -1;
+    }
+
+    EncodingType type = getEncodingType(argv[1]);
+    if (type == ETUnknown) {
+        cout << "Błędny tryb szyfrowania\n";
+        return -1;
+    }
+
+    char *keystorePath = argv[2];
+
+    int index = atoi(argv[3]);
+
+    char *filePath = argv[4];
+
+    EncodingEffect effect = getEncodingEffect(argv[5]);
+
     char *password = getpass("Podaj hasło: ");
-    KeyChain *keyChain = new KeyChain((unsigned char *) "/Users/marian/Desktop/ssl/keyChain.kc",
-                                      (unsigned char *) password, 0);
+    KeyChain *keyChain = new KeyChain((unsigned char *) keystorePath,
+                                      (unsigned char *) password, index);
     unsigned char *key = keyChain->key;
     FileContent *inputContent = new FileContent(false);
     inputContent->readFromPath("/Users/marian/Desktop/ssl/test1.txt");
@@ -28,5 +64,25 @@ int main() {
     delete outputContent;
     delete keyChain;
     return 0;
+}
+
+EncodingEffect getEncodingEffect(char *effectASCII) {
+    int effect = atoi(effectASCII);
+
+    switch (effect) {
+        case 0: return EEEncoding;
+        case 1: return EEDecoding;
+        default: return EEUnknown;
+    }
+}
+
+EncodingType getEncodingType(char *encodingType) {
+    if (strcmp(encodingType, "CBC") == 0) {
+        return ETCBC;
+    } else if (strcmp(encodingType, "CTR") == 0) {
+        return ETCTR;
+    } else {
+        return ETUnknown;
+    }
 }
 
